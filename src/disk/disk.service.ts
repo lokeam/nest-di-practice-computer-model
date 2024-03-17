0ab2cdc6f9e61a0d18f8f07778e8524d7d388117
Ahn Ming Loke <lokeahnming@gmail.com>
2024-03-17 19:01:24 -0400

    feat: Connect test fn on CPU service making use of method on PowerService

--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) {}
+
+  getData() {
+    console.log('Drawing 3 watts of power from Power Service');
+    this.powerService.supplyPower(3);
+
+    return 'here is the data';
+  }
+}
